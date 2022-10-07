81539e1bd8377f21f7b8f9feafbc414bee6b6c5f
Neville Dempsey <NevilleD.debian@sgr-a.net>
2022-10-08 09:37:04 +1300

    Add sample Sieve_of_Eratosthenes app with DICT

---
+++ bin/build_sub_prelude.sh
@@ -0,0 +1,18 @@
+if [ "$#" = 0 ]; then
+    set -- "base=val sub=item" "base=val sub=string"
+fi
+
+for base_sub in "$@"; do
+    eval $base_sub
+    eval `echo $base_sub | tr "[a-z]" "[A-Z]"`
+    # echo $base $BASE $sub $SUB
+    for from in prelude/*_$base"_"*.a68; do
+        eval `echo "to=sub_$from" | sed "s/_$base""_/_$sub""_/g"`
+        if [ "$from" -ot "$to" ]; then
+            echo "$from older than $to - skipping build"
+        else
+            echo "$from newer than $to - building"
+            sed "s/$BASE\b/$SUB/g; s/\b $base##/ $sub##/g" < "$from" > "$to"
+        fi
+    done
+done
